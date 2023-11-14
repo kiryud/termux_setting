86e2ea5ac9be12da7815da97b3979d4ec954e44c
jijeong <100929327+kiryud@users.noreply.github.com>
2023-11-14 23:07:15 +0900

    Update setft.sh

--- setft.sh
+++ setft.sh
@@ -1,28 +1,17 @@
 #! /bin/bash
 
-# 안드로이드 저장공간 접근 권한 획득
-echo "안드로이드 저장공간 접근 권한 획득"
-termux-setup-storage
-
-# pkg 업데이트 및 업그레이드
-echo "pkg 업데이트 및 업그레이드"
-pkg update && pkg upgrade -y
+# apk 업데이트 및 업그레이드
+echo "apk 업데이트 및 업그레이드"
+apk update && apk upgrade -y
 
 # 기본적인 도구 설치
-echo "기본적인 도구 설치 vim openssh"
-pkg install -y vim openssh apt aptitude
-
-# proot-distro 설치
-echo "proot-distro 설치"
-apt update
-apt upgrade -y
-apt install proot-distro
-
+echo "기본적인 도구 설치 vim openssh aptitude python3"
+apk install -y vim openssh aptitude python3
 
 # 42header clone
 echo "42header clone"
 git clone git@github.com:42Paris/42header.git
 
-# norminette clone
-echo "norminette clone"
-git clone git@github.com:42Paris/norminette-v3.git
+# norminette
+echo "norminette"
+
