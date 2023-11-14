97ac638d4534b2b80d4b19490377eca7458da10a
jijeong <100929327+kiryud@users.noreply.github.com>
2023-11-14 23:18:47 +0900

    Update setft.sh

--- setft.sh
+++ setft.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-# apk 업데이트 및 업그레이드
-echo "apk 업데이트 및 업그레이드"
-apk update && apk upgrade -y
+# apt 업데이트 및 업그레이드
+echo "apt 업데이트 및 업그레이드"
+apt update && apk upgrate -y
 
 # 기본적인 도구 설치
-echo "기본적인 도구 설치 vim openssh aptitude"
-apk install -y vim openssh aptitude
+echo "기본적인 도구 설치 vim openssh aptitude python3"
+apt install -y vim openssh aptitude python3
 
 # 42header clone
 echo "42header clone"
