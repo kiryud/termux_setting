357fa32b9e3077d75506d945a9f78c5b387d01a4
jijeong <100929327+kiryud@users.noreply.github.com>
2023-11-14 23:43:18 +0900

    Update setft.sh

--- setft.sh
+++ setft.sh
@@ -8,6 +8,11 @@ apt update && apk upgrate -y
 echo "기본적인 도구 설치 vim openssh aptitude python3"
 apt install -y vim aptitude python3
 
+# 기본적인 세팅 복사
+echo "기본적인 세팅 복사"
+cp setting/.bashrc ~/.bashrc
+cp setting/.vimrc ~/.vimrc
+
 # 42header clone
 echo "42header clone"
 git clone git@github.com:42Paris/42header.git
