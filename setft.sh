37e2ecd7f7f6dacbee9051490f21cc0f6080e7ad
jijeong <100929327+kiryud@users.noreply.github.com>
2023-11-15 00:57:31 +0900

    Update setft.sh

--- setft.sh
+++ setft.sh
@@ -5,8 +5,8 @@ echo "apt 업데이트 및 업그레이드"
 apt update && apk upgrate -y
 
 # 기본적인 도구 설치
-echo "기본적인 도구 설치 vim aptitude make cmake python3.9"
-apt install -y vim aptitude make cmake python3.9
+echo "기본적인 도구 설치 vim aptitude make cmake python3"
+apt install -y vim aptitude make cmake python3 python3-pip python3-venv
 
 # 기본적인 세팅 복사
 echo "기본적인 세팅 복사"
@@ -19,10 +19,3 @@ git clone git@github.com:42Paris/42header.git
 mkdir ~/.vim
 mkdir ~/.vim/plugin
 cp 42header/plugin/stdheader.vim ~/.vim/plugin/stdheader.vim
-
-# norminette
-echo "install norminette"
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py
-python3 -m pip install --upgrade pip setuptools
-python3 -m pip install norminette
