01dabba49081730cab83a5ad3cab804a58203582
jijeong <100929327+kiryud@users.noreply.github.com>
2023-11-14 23:12:25 +0900

    Update setft.sh

--- setft.sh
+++ setft.sh
@@ -5,13 +5,15 @@ echo "apk 업데이트 및 업그레이드"
 apk update && apk upgrade -y
 
 # 기본적인 도구 설치
-echo "기본적인 도구 설치 vim openssh aptitude python3"
-apk install -y vim openssh aptitude python3
+echo "기본적인 도구 설치 vim openssh aptitude"
+apk install -y vim openssh aptitude
 
 # 42header clone
 echo "42header clone"
 git clone git@github.com:42Paris/42header.git
 
 # norminette
-echo "norminette"
-
+echo "install norminette"
+python3 -m pip install --upgrade norminette
+python3 -m pip install --upgrade pip setuptools
+python3 -m pip install norminette
