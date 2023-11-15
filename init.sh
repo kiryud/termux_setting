cc855b8ec4ad41286941963f3a0951e81ec50bdd
jijeong <100929327+kiryud@users.noreply.github.com>
2023-11-15 13:03:35 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,10 @@
 echo "안드로이드 저장공간 접근 권한 획득"
 termux-setup-storage
 
+# termux 레포지토리 변경
+echo "termux 레포지토리 변경"
+termux-change-repo
+
 # pkg 업데이트 및 업그레이드
 echo "pkg 업데이트 및 업그레이드"
 pkg update && pkg upgrade -y
