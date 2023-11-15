0a06488d07296b70c5a638de3878a43e32c4eb06
jijeong <100929327+kiryud@users.noreply.github.com>
2023-11-15 13:05:33 +0900

    Update setft.sh

--- setft.sh
+++ setft.sh
@@ -5,7 +5,7 @@ echo "apt 업데이트 및 업그레이드"
 apt update && apk upgrate -y
 
 # 기본적인 도구 설치
-echo "기본적인 도구 설치 vim aptitude make cmake python3"
+echo "기본적인 도구 설치 vim aptitude make cmake python3 (pip, venv)"
 apt install -y vim aptitude make cmake python3 python3-pip python3-venv
 
 # 기본적인 세팅 복사
